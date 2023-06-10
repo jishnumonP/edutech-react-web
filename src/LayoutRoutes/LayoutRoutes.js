58114f50c13ce3b243e136e075a4851bbc246a68
jishnumonP <jishnup981@gmail.com>
2023-06-10 17:29:12 +0530

    new

--- src/LayoutRoutes/LayoutRoutes.js
+++ src/LayoutRoutes/LayoutRoutes.js
@@ -14,6 +14,7 @@ function LayoutRoutes() {
     <Router>
       <Header />
       <Routes>
+      <Route path='/' element={<Home />} />
         <Route path='/edutech-web' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/plans' element={<Plans />} />
